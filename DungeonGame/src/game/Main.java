c79f18aa34585614fb91c9aeccfe80a624276eed
Reece <Reece@DESKTOP-VCTRSCT>
2016-10-03 11:45:08 +0100

    main in progress

--- DungeonGame/src/game/Main.java
+++ DungeonGame/src/game/Main.java
@@ -1,9 +1,19 @@
 package game;
 
-public class Main {
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.image.VolatileImage;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
 
 	private boolean running;
+	private Dungeon dungeon;
+	private VolatileImage offimage;
 
+	public static int width, height;
+	
 	private void run() {
 		
 		init();
@@ -23,14 +33,28 @@ public class Main {
 
 	private void init() {
 		running = true;
+		dungeon = new Dungeon();
+		
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		width = dim.width;
+		height = dim.height;
+		
+		this.setTitle("Dungeon Game");
+		this.setSize(dim);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setUndecorated(true);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		this.setVisible(true);
+		
+		
 	}
 
 	private void update(long nanoTime) {
-
+		dungeon.update(nanoTime);
 	}
 
 	private void draw() {
-
+		dungeon.draw(g);
 	}
 
 	public static void main(String[] args) {
