2c275e0abb9bc90bdcd397167737d5fb077534cb
Reece <Reece@DESKTOP-VCTRSCT>
2016-10-03 11:09:19 +0100

    added .pro and .class to gitignore

--- DungeonGame/src/game/Main.java
+++ DungeonGame/src/game/Main.java
@@ -2,4 +2,35 @@ package game;
 
 public class Main {
 
+	private boolean running;
+
+	private void run() {
+		
+		init();
+		
+		while (running) {
+			update();
+			if (running) {
+				draw();
+			}
+		}
+	}
+
+	private void init() {
+		running = true;
+	}
+
+	private void update() {
+
+	}
+
+	private void draw() {
+
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.run();
+	}
+
 }
