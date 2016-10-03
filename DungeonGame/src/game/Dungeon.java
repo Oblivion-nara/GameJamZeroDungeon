8303a2fd898b4091959614832197e703ce8fd345
Reece <Reece@DESKTOP-VCTRSCT>
2016-10-03 17:33:22 +0100

    movement in dungeon is fully functional

--- DungeonGame/src/game/Dungeon.java
+++ DungeonGame/src/game/Dungeon.java
@@ -3,31 +3,52 @@ package game;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
-
-import javax.swing.JPanel;
+import java.awt.image.BufferedImage;
 
 import utils.Block;
-import utils.InputHandler;
 import utils.NoiseGenerator;
 
 public class Dungeon {
 
 	private static final long serialVersionUID = 1L;
+	private float xDif, yDif;
 	private boolean[][] dungeon;
+	private BufferedImage mapPicture;
 	private Block wall = new Block("stone/rock.png", 0, true, 16, 16);
 	private Block floor = new Block("stone/stone.png", 0, false, 16, 16);
 	private NoiseGenerator noise;
 	private Player player;
-	private float xDif, yDif;
 
 	public Dungeon() {
 		noise = new NoiseGenerator(0);
-		player = new Player(1000,1000,100, 1, 1, 50);
-		dungeon = noise.getCellularAutomataNoise(256, 256, 4, 4, 4);
+		initDungeon();
+		player = new Player(1000, 1000, 100, 1, 1, 50);
 	}
 
-	private void movePlayer(float time){
+	private void initDungeon() {
+
+		int x = 256;
+		int y = 256;
+		int tileSize = 16;
+
+		dungeon = noise.getCellularAutomataNoise(x, y, 4, 4, 4);
+		mapPicture = new BufferedImage(tileSize * x, tileSize * y, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g = (Graphics2D) mapPicture.getGraphics();
+
+		for (int i = 0; i < dungeon.length; i++) {
+			for (int j = 0; j < dungeon[0].length; j++) {
+				if (dungeon[i][j]) {
+					floor.draw(g, i * tileSize, j * tileSize);
+				} else {
+					wall.draw(g, i * tileSize, j * tileSize);
+				}
+			}
+		}
+	}
+
+	private void movePlayer(float time) {
 		if (Main.input.isKeyDown(KeyEvent.VK_W)) {
 			if (Main.input.isKeyDown(KeyEvent.VK_A)) {
 				player.moveUL(time);
@@ -50,12 +71,12 @@ public class Dungeon {
 			player.moveRight(time);
 		}
 	}
-	
-	private void updateCamera(){
-		xDif = -(player.xLocation - Main.width/2);
-		yDif = -(player.yLocation - Main.height/2);
+
+	private void updateCamera() {
+		xDif = -(player.xLocation - Main.width / 2);
+		yDif = -(player.yLocation - Main.height / 2);
 	}
-	
+
 	public void update(float time) {
 		movePlayer(time);
 		updateCamera();
@@ -64,17 +85,10 @@ public class Dungeon {
 	public void draw(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setColor(Color.black);
-		g2d.fillRect(0, 0, 2048, 2048);
-		g2d.setColor(Color.white);
-		for (int i = 0; i < dungeon.length; i++) {
-			for (int j = 0; j < dungeon[0].length; j++) {
-				if (dungeon[i][j]) {
-					floor.draw(g2d, i * 16 + (int) xDif, j * 16 + (int) yDif);
-				} else {
-					wall.draw(g2d, i * 16 + (int) xDif, j * 16 + (int) yDif);
-				}
-			}
-		}
+		g2d.fillRect(0, 0, Main.width, Main.height);
+		g2d.drawImage(mapPicture, (int) xDif, (int) yDif, mapPicture.getWidth(), mapPicture.getHeight(), null);
+		player.draw(g2d);
+
 	}
 
 }
