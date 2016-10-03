bdf679876d2e26be6795aba740f9ece30141599e
mxw596 <mxw596@cca-ug04-15070.cs.bham.ac.uk>
2016-10-03 11:43:16 +0100

    dungeon changes

--- DungeonGame/src/game/Dungeon.java
+++ DungeonGame/src/game/Dungeon.java
@@ -2,34 +2,57 @@ package game;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JPanel;
 
+import utils.Block;
+import utils.InputHandler;
 import utils.NoiseGenerator;
 
 public class Dungeon extends JPanel{
 	
 	private static final long serialVersionUID = 1L;
 	private boolean[][] dungeon;
-	private NoiseGenerator noise = new NoiseGenerator(0);
-	private int xDif = 0, yDif = 0;
+	private Block wall = new Block("rock.png", 0, true, 16, 16);
+	private Block floor = new Block("stone.png", 0, false, 16, 16);
+	private NoiseGenerator noise;
+	private InputHandler input;
+	private float xDif, yDif;
 	
 	public Dungeon(){
+		input = new InputHandler(this);
+		noise = new NoiseGenerator(0);
 		dungeon = noise.getCellularAutomataNoise(256, 256, 4, 3, 5);
 	}
 	
-	public void update(){
-		
+	public void update(long time){
+		if(input.isKeyDown(KeyEvent.VK_W)){
+			yDif += player.speed * time / Math.sqrt(2);
+		}
+		if(input.isKeyDown(KeyEvent.VK_A)){
+			xDif += player.speed * time / Math.sqrt(2);
+		}
+		if(input.isKeyDown(KeyEvent.VK_S)){
+			yDif -= player.speed * time / Math.sqrt(2);
+		}
+		if(input.isKeyDown(KeyEvent.VK_D)){
+			xDif -= player.speed * time / Math.sqrt(2);
+		}
 	}
 	
 	public void draw(Graphics g){
-		g.setColor(Color.black);
-		g.fillRect(0, 0, 2048, 2048);
-		g.setColor(Color.white);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setColor(Color.black);
+		g2d.fillRect(0, 0, 2048, 2048);
+		g2d.setColor(Color.white);
 		for(int i = 0; i < dungeon.length; i++){
 			for(int j = 0; j < dungeon[0].length; j++){
 				if(dungeon[i][j]){
-					g.fillRect(i*16 + xDif, j*16 + yDif, 16, 16);
+					floor.draw(g2d, i * 16 + (int) xDif, j * 16 + (int) yDif);
+				}else{
+					wall.draw(g2d, i * 16 + (int) xDif, j * 16 + (int) yDif);
 				}
 			}
 		}
